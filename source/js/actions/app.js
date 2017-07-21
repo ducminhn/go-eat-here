4bbe591c651dce7a9edcf4b40eb34f3b40e2c477
Andrew Hayter <andrewhayter@gmail.com>
2017-07-21 16:35:10 -0700

    dynamic location

--- source/js/actions/app.js
+++ source/js/actions/app.js
@@ -2,8 +2,8 @@ import api from '../api';
 
 export const SEARCH_RESTAURANTS = 'SEARCH_RESTAURANTS';
 
-export function searchRestaurants(offset){
-  const promise = api.getZomatoRestaurants(offset);
+export function searchRestaurants(offset, latitude, longitude){
+  const promise = api.getZomatoRestaurants(offset, latitude, longitude);
   return{
     type: SEARCH_RESTAURANTS,
     payload: promise
--- source/js/api/index.js
+++ source/js/api/index.js
@@ -4,15 +4,15 @@ import axios from 'axios';
 const DATA_URL = "https://developers.zomato.com/api/v2.1/search";
 const USER_KEY = "f7b6334db7c203422f364e4498a59813";
 
-function getZomatoRestaurants(offset) {
+function getZomatoRestaurants(offset, latitude, longitude, radius=1500) {
   const promise = axios.request({
     url: DATA_URL,
     params: {
       start: offset,
       count: 20,
-      lat: 49.246292,
-      lon: -123.116226,
-      radius: 5000
+      lat: latitude,
+      lon: longitude,
+      radius: radius
     },
     method: 'get',
     headers: {'user-key': USER_KEY}
